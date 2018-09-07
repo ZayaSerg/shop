ee265aad16f713f5889954497a2717138a8c60b0
zayaserg <zayaserg@gmail.com>
2018-09-07 09:32:10 +0300

    #3.1 Search Form

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,23 @@
-$(document).ready(function () {
-    svg4everybody({});
+$(document).ready(function() {
+  // alert("JQuery working");
+  svg4everybody({});
+  // console.log(this);
+
+  let openSearchForm = () => {
+    $(document).on("click", ".search__icon", function() {
+      $(this)
+        .parent()
+        .addClass("search--open");
+    });
+  };
+
+  let clearSearchForm = () => {
+    $(document).on("click", ".search__clear", function() {
+      $(".search__input").val("");
+    });
+  };
+
+  openSearchForm();
+  clearSearchForm();
+  // GetStarted();
 });
